e87e8e6068172038e2321c7cdc8c401801074701
Madhuri Palanivelu <palsmadhu18@gmail.com>
2019-11-12 09:44:27 -0800

    added GreeceFlag, README.md and blank Test

---
+++ test/ImageDataTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+
+/**
+ * No test cases were required for our assignment as all outputs were visially representated by,
+ * images present in the res folder.
+ */
+public class ImageDataTest {
+
+}
