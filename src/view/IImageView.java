27392d54d59b0d9031296e0becabed646efc7e20
SanaJahan <sanadgrt@gmail.com>
2019-12-01 14:28:53 -0800

    started view

--- src/view/IImageView.java
+++ src/view/IImageView.java
@@ -1,5 +1,13 @@
 package view;
 
+import java.awt.event.ActionListener;
+
 public interface IImageView {
+
+  void loadImage();
+  void drawImage();
+  void saveImage();
+  void generateImage();
+  void setListener(ActionListener listener);
 
 }
