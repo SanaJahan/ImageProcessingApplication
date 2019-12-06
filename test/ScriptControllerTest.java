6475f50138ab02e28aa3914585c1da29443a16c1
SanaJahan <sanadgrt@gmail.com>
2019-12-05 21:32:16 -0800

    code refactoring

--- test/ScriptControllerTest.java
+++ test/ScriptControllerTest.java
@@ -7,40 +7,34 @@ import controller.ScriptController;
 
 public class ScriptControllerTest {
 
-  private ScriptController scriptController;
-
-  @Before
-  public void setUp() {
-    scriptController = new ScriptController();
-  }
 
   // test for load fail and pass
   @Test(expected = IllegalArgumentException.class)
   public void loadFailed() throws IOException {
-    scriptController.processTerms(new String[]{"load","image-not-there"});
+    ScriptController.processTerms(new String[]{"load","image-not-there"});
   }
 
   // test for generate flag
   @Test
   public void testGenerateFlag() throws IOException {
-    scriptController.processTerms(new String[]{"generate","flag","greece","200","300"});
-    scriptController.processTerms(new String[]{"save", "greece-flag.png"});
+    ScriptController.processTerms(new String[]{"generate","flag","greece","200","300"});
+    ScriptController.processTerms(new String[]{"save", "greece-flag.png"});
   }
 
   // test for vibgyor
   @Test
   public void testVibgyor() throws IOException {
-    scriptController.processTerms(new String[]{"generate","vibgyor","horizontal","500","400"});
-    scriptController.processTerms(new String[]{"save", "horizontal.png"});
+    ScriptController.processTerms(new String[]{"generate","vibgyor","horizontal","500","400"});
+    ScriptController.processTerms(new String[]{"save", "horizontal.png"});
 
   }
 
   // test for blur
   @Test
   public void testBlur() throws IOException {
-    scriptController.processTerms(new String[]{"load","cat.png"});
-    scriptController.processTerms(new String[]{"blur"});
-    scriptController.processTerms(new String[]{"save", "catblur.png"});
+    ScriptController.processTerms(new String[]{"load","cat.png"});
+    ScriptController.processTerms(new String[]{"blur"});
+    ScriptController.processTerms(new String[]{"save", "catblur.png"});
   }
 
 }
